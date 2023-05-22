0906d1b090c9c486ce3e086b0b9740ea147fee1a
TruongTho3345 <100506191+TruongTho3345@users.noreply.github.com>
2023-05-22 20:15:35 +0700

    Create new project

---
+++ src/component/index.tsx
@@ -0,0 +1,37 @@
+import axios, { AxiosResponse } from "axios";
+import React, { FC, useEffect, useState } from "react";
+import { Movies } from "../models/movies";
+interface MoviesProps {
+  api: string;
+}
+
+const MoviesComponent: FC<MoviesProps> = ({ api }) => {
+  const [movies, setMovies] = useState<Movies[]>([]);
+
+  const handleFetchMoviesSuccess = (response: AxiosResponse<Movies[]>) => {
+    setMovies(response.data);
+  };
+
+  const fetchMovie = () => {
+    // promise
+    axios.get(api).then(handleFetchMoviesSuccess);
+  };
+
+  useEffect(fetchMovie, []);
+
+  return (
+    <div>
+      {movies.map((movie: Movies) => {
+        return (
+          <div className="movie">
+            <h2>{movie.name}</h2>
+            <img src={movie.image} width={300} alt="" />
+            <h5>{movie.actorName}</h5>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MoviesComponent;
